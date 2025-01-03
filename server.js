f0813b5f59cbf333a5452e2486262582166c3466
Tayo Carvalhal <root@hbvu.su>
2025-01-03 11:43:35 +0100

    added function to return array of posts

--- server.js
+++ server.js
@@ -4,20 +4,70 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs').promises;
 var debug = require('debug')('blogt-api:server');
+
+const {
+  findLatestPost,
+  getNext,
+  getPrev,
+  formatDate
+} = require('./utils/utils');
+
 const postsDir = path.join(__dirname, 'posts');
-const corsProperties = 
-  {
-    origin: 'http://localhost:5173', // Your Vue app's development server
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type']
-  }
+const corsProperties =
+{
+  origin: 'http://localhost:5173', // Your Vue app's development server
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}
 
 // Create an instance of an Express application
 const app = express();
 app.use(cors(corsProperties));
+
 // Define a route for GET requests to "/"
-app.get('/', (req, res) => {
-  res.send('OK'); // Respond with "OK"
+app.get('/', async (req, res) => {
+  try {
+    const { latestPostPath, latestPostDate } = await findLatestPost();
+    var dateString = await formatDate(latestPostDate);
+    debug(`[MAIN] Latest post date: ${latestPostDate}`)
+
+    if (!latestPostPath) {
+      return res.status(404).json({ error: 'No posts found' });
+    }
+
+    const postsArray = [];
+    const postsPerPage = 5; // Number of posts per page
+
+    // Loop to get the posts for the requested page
+    for (let i = 0; i < postsPerPage; i++) {
+      const year = dateString.slice(0, 4);
+      const month = dateString.slice(4, 6);
+      const day = dateString.slice(6, 8);
+      let filePath = path.join(__dirname, '.', 'posts', year, month, `${day}.md`);
+
+      try {
+        const data = await fs.readFile(filePath, 'utf-8');
+        postsArray.push(data);
+        dateString = await getPrev(dateString)
+        if (!dateString) {
+          break;
+        } else {
+          debug("Posts to display" + postsArray.length)
+        }
+      } catch (err) {
+        console.error(`No post found for ${year}-${month}-${day}`);
+        dateString = await getPrev(dateString)
+        // Continue to next date if file does not exist
+      }
+    }
+    res.send(postsArray);
+
+  }
+  catch (error) {
+    console.error('Error fetching posts:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+
 });
 
 app.get('/:dateString', async (req, res) => {
@@ -58,5 +108,5 @@ const PORT = 3001;
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
-  debug(`Server is running on ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
