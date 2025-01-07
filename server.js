73f6bb9b095c4e681d39c7613a7de466bf193321
Tayo Carvalhal <root@hbvu.su>
2025-01-07 11:29:32 +0100

    tags route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const path = require('path');
 const fs = require('fs').promises;
 var debug = require('debug')('blogt-api:server');
 
+let filePath = path.join(__dirname, '.');
+const tagIndex = require(`${filePath}/tags_index.json`); // Path to JSON tag index file
+debug(Object.keys(tagIndex).length);
+
 const {
   findLatestPost,
   getNext,
@@ -69,6 +73,24 @@ app.get('/', async (req, res) => {
   }
 
 });
+
+app.get('/tags/:tagName', async (req, res) => {
+  var { tagName } = req.params;
+  tagName = decodeURIComponent(tagName); // Decode the tag name to handle multi-word and special characters
+
+  const normalizedTag = tagName.toLowerCase();
+
+  let postFiles = tagIndex[normalizedTag] || [];
+  debug(`${tagName} >> ${postFiles.length}`);
+
+  if (postFiles.length === 0) {
+      return res.send('noPosts', { tagName });
+  } else {
+    res.send(postFiles);
+  }
+});
+
+
 
 app.get('/:dateString', async (req, res) => {
   const { dateString } = req.params;
