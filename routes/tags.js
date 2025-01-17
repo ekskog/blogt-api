2c5dcf39faf82fc0dd8b9e451451b26f7689480f
Tayo Carvalhal <root@hbvu.su>
2025-01-17 12:31:23 +0100

    better handling of get tags

--- routes/tags.js
+++ routes/tags.js
@@ -1,15 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const path = require('path');
-var debug = require('debug')('blot-too:tags-route');
+const path = require("path");
+var debug = require("debug")("blot-too:tags-route");
 
-let filePath = path.join(__dirname, '..', 'posts');
+let filePath = path.join(__dirname, "..", "posts");
 const tagIndex = require(`${filePath}/tags_index.json`); // Path to JSON tag index file
 debug(Object.keys(tagIndex).length);
 
 /* GET users listing. */
-router.get('/:tagName', async (req, res) => {
+router.get("/:tagName", async (req, res) => {
   var { tagName } = req.params;
   tagName = decodeURIComponent(tagName); // Decode the tag name to handle multi-word and special characters
 
@@ -18,11 +18,7 @@ router.get('/:tagName', async (req, res) => {
   let postFiles = tagIndex[normalizedTag] || [];
   debug(`${tagName} >> ${postFiles.length}`);
 
-  if (postFiles.length === 0) {
-      return res.send('noPosts', { tagName });
-  } else {
-    res.send(postFiles);
-  }
+  res.send(postFiles);
 });
 
 module.exports = router;
